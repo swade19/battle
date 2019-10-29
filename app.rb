3d9661f41def420136a42dc65dfa5e14963a2aa7
Student <student@Admins-MacBook-Air.local>
2019-10-29 16:40:36 +0000

    green test

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require "sinatra/base"
 
 class Battle < Sinatra::Base
   get "/" do
-    "Hello Battle!"
+    erb :index
+  end
+
+  post "/names" do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
   end
   run! if app_file == $0
 end
